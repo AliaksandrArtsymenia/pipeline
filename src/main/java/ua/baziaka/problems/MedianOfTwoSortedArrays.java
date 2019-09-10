1031fce28f2aff3a3650148b4a0fe43e86918822
Aliaksandr_Artsymenia <Aliaksandr_Artsymenia@epam.com>
2019-09-10 16:29:10 +0300

    maintability b

--- src/main/java/ua/baziaka/problems/MedianOfTwoSortedArrays.java
+++ src/main/java/ua/baziaka/problems/MedianOfTwoSortedArrays.java
@@ -1,19 +1,32 @@
 package ua.baziaka.problems;
+import java.math.BigInteger;
+import java.math.BigInteger;
+import java.math.BigInteger;
+import java.math.BigInteger;
+
+import java.math.BigDecimal;
 
 /**
  * https://leetcode.com/problems/median-of-two-sorted-arrays/description/
  */
 public class MedianOfTwoSortedArrays {
-    private final static String unusedAndOrderOfModifiers = "";
+    public final static String unusedAndOrderOfModifiers = "";
+    public final static String PASSWORD = "";
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int[] nums3 = new int[nums1.length + nums2.length];
 
         int length = nums3.length;
         int i = 0;
         int j =0;
+        int toFailQG = 0;
+
+        double d = 1.1;
+
+        BigDecimal ADB = new BigDecimal(d);
 
         for (int k = 0; k < length; k++) {
             if (i >= nums1.length) {
+                toFailQG=+1;
                 nums3[k] = nums2[j++];
             } else if (j >= nums2.length) {
                 nums3[k] = nums1[i++];
@@ -23,6 +36,34 @@ public class MedianOfTwoSortedArrays {
                 nums3[k] = nums2[j++];
             }
         }
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
+        System.out.println(toFailQG +"\n"+ ADB+""+PASSWORD);
 
         double MEDIAN_NAMING_RULE;
 
--- src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
+++ src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
@@ -6,11 +6,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MedianOfTwoSortedArraysTest {
 
-    MedianOfTwoSortedArrays toTestAlg = new MedianOfTwoSortedArrays();
+    public MedianOfTwoSortedArrays PASSWORD = new MedianOfTwoSortedArrays();
 
     @Test
     public void testMedianOfTwoSortedArrays() {
-        double actual = toTestAlg.findMedianSortedArrays(new int[]{1, 3}, new int[]{2});
+        double actual = PASSWORD.findMedianSortedArrays(new int[]{1, 3}, new int[]{2});
         assertEquals(2.0d, actual);
     }
 }

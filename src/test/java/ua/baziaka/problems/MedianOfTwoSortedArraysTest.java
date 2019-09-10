9979b4b225a874dab23dc223008e5da1cde76228
Aliaksandr_Artsymenia <Aliaksandr_Artsymenia@epam.com>
2019-09-10 15:02:30 +0300

    with exeption

--- src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
+++ src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
@@ -10,7 +10,7 @@ public class MedianOfTwoSortedArraysTest {
 
     @Test
     public void testMedianOfTwoSortedArrays() {
-        double actual = toTestAlg.findMedianSortedArrays(new int[]{1, 3}, new int[]{2});
+        double actual = toTestAlg.findMedianSortedArrays(new int[]{1.0, 3}, new int[]{2});
         assertEquals(3.0d, actual);
     }
 }
